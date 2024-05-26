63c7f949c2bb796f06f78b61f9d6db2eb76b045f
rbral <rbral@student.touro.edu>
2024-05-26 17:09:18 -0400

    part 1: all tests pass

--- src/main/java/bral/museum/RijksService.java
+++ src/main/java/bral/museum/RijksService.java
@@ -1,69 +1,33 @@
 package bral.museum;
 
-/*import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import java.io.IOException;*/
+import bral.museum.json.ArtObjects;
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
-/*
-Instructions:
-You will need to create a RijksService that has 3 methods.
-You can hard code the English version in the url’s culture field.
-You must pass the api key with each request.
-1. A request to the collection with no search parameters but the page number (p field)
-2. A request to the collection with a query (q field) and page number
-3. A request to the collection with a query for artist (involvedMaker field) and page number.
-
-Write tests for all 3 requests.
-
-The data you’re going to need to retrieve from the API call are
-The “artObjects”
-Each art object has a title, longTitle, webImage and principalOrFirstMaker
+/**
+ * https://www.rijksmuseum.nl/
  */
-public class RijksService
-{
-    public RijksService getService()
-    {
+public interface RijksService {
+    @GET("/api/en/collection")
+    Single<ArtObjects> getCollectionByPage(
+            @Query("key") String apiKey,
+            @Query("p") int page
+    );
 
-        return null;
-    }
+    @GET("/api/en/collection")
+    Single<ArtObjects> searchCollectionByQuery(
+            @Query("key") String apiKey,
+            @Query("q") String query,
+            @Query("p") int page
+    );
 
-    // chat gpt:
+    @GET("/api/en/collection")
+    Single<ArtObjects> searchCollectionByArtist(
+            @Query("key") String apiKey,
+            @Query("principalOrFirstMaker") String artist,
+            @Query("p") int page
+    );
 
-/*
-    public JsonNode getCollection(int page) throws IOException {
-        String url = String.format("%s?p=%d&key=%s", BASE_URL, page, API_KEY);
-        return sendRequest(url);
-    }
 
-    public JsonNode searchCollection(String query, int page) throws IOException {
-        String url = String.format("%s?q=%s&p=%d&key=%s", BASE_URL, query, page, API_KEY);
-        return sendRequest(url);
-    }
-
-    public JsonNode searchByArtist(String artist, int page) throws IOException {
-        String url = String.format("%s?involvedMaker=%s&p=%d&key=%s", BASE_URL, artist, page, API_KEY);
-        return sendRequest(url);
-    }
-
-    private JsonNode sendRequest(String url) throws IOException {
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpGet request = new HttpGet(url);
-            try (CloseableHttpResponse response = httpClient.execute(request)) {
-                ObjectMapper mapper = new ObjectMapper();
-                return mapper.readTree(response.getEntity().getContent());
-            }
-        }
-    }*/
 }
-
-
-
-
-
-
-
-
